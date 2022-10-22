31da4d24cef2ed9ca681d2a6839c5e487a24f6c5
arinach <arina.tchukurova@yandex.ru>
2022-10-22 23:40:49 +0300

    add first

---
+++ first.py
@@ -0,0 +1,10 @@
+# Вычислить число c заданной точностью d 
+ 
+first_number = float(input()) 
+d = float(input()) 
+ 
+summ = first_number+d 
+result = round(summ,2) 
+print(result) 
+ 
+#Вроде правильно поняла задачу..
